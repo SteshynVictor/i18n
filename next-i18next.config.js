c43fd31fe5daf2a64f3608d1af8d3a68ec8c31fc
Webmarker <steshynvictor@gmail.com>
2023-10-31 12:46:28 +0100

    first commit

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+// https://nextjs.org/docs/pages/building-your-application/routing/internationalization
+
+module.exports = {
+    i18n: {
+      defaultLocale: 'uk',
+      locales: ['en', 'uk', 'ru'],
+      localeDetection: false,
+    },
+  }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+import { appWithTranslation } from 'next-i18next'
+
+const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
+
+export default appWithTranslation(App);
